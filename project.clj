37bf4c3945c62ce4579cf88b2dab8fb1655fa32c
Dane Johnson <daneallenjohnson@protonmail.com>
2020-01-22 12:33:00 -0600

    Properly add sonatype snapshot database

--- project.clj
+++ project.clj
@@ -6,10 +6,8 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [edu.cmu.sphinx/sphinx4-core "5prealpha-SNAPSHOT"]
                  [edu.cmu.sphinx/sphinx4-data "5prealpha-SNAPSHOT"]]
-  :mirrors {"sonatype" {:name "sonatype"
-                        :url "https://oss.sonatype.org/content/repositories/snapshots"}
-            "central" {:name "central"
-                       :url "https://repo1.maven.org/maven2/"}}
+  :repositories [["sonatype" {:url "https://oss.sonatype.org/content/repositories/snapshots"
+                              :releases false}]]
   :main ^:skip-aot alita.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
